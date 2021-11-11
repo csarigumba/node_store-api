a10d6d4eeb25cbff909fde5d95124cf5e9cc1202
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-11 19:07:54 +0800

    added sorting

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,12 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' });
+  const products = await Product.find({}).sort('-price');
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -19,7 +20,17 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  // sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
