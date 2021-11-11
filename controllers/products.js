b2e8ec073591df37e76a6751cf053f1e9f9ab66f
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-11 18:49:27 +0800

    specific search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,26 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: 'products testing route' });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
