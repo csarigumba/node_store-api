3b65b18a8f6b8ad831a3f34c05541981537c8196
Cedric Sarigumba <cediesarigumba@gmail.com>
2021-11-12 19:49:34 +0800

    Added advanced filtering

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -15,9 +15,26 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(regEx, match => `-${operatorMap[match]}-`);
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach(item => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
 
   let result = Product.find(queryObject);
